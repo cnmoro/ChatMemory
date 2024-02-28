a9240088e42b6356b1e1e2d19e74bb65b7821735
Carlo Nery de Lima Moro <carlo.moro@wises.com.br>
2024-02-28 19:04:36 -0300

    Update version to 0.3 in pyproject.toml and setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='chatmemorydb',
-    version='0.2',
+    version='0.3',
     author='Carlo Moro',
     author_email='cnmoro@gmail.com',
     description="Memory",
